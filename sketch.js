c50015631790b47fb3695760adccafad68f2a747
Guilherme <141414294+guilhermee15@users.noreply.github.com>
2023-08-11 08:18:29 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(750,750);
+  background('green');
+}
+
+function draw() {
+  
+  
+  stroke('red');
+  fill('gold');
+if(mouseIsPressed){
+  rect(mouseX,mouseX,30,30);
+  
+  
+}
+}
